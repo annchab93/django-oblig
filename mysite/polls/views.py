5146466de2b1f7c397731ce964c4cc13fcc24261
annchab93 <Anncha@Ann-Charlotts-MacBook-Air.local>
2015-03-10 15:36:54 +0100

    tamplates rot

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -5,17 +5,17 @@ from django.http import HttpResponse
 from polls.models import Question 
 
 
+
 def index(request):
-	latest_question_list = Question.objects.order_by('-pub_date')[:5]
-	template = loader.get_template('polls/index.html')
-    context = RequestContext(request, {
-        'latest_question_list': latest_question_list,
-    })
-    return HttpResponse(template.render(context))
+    latest_question_list = Question.objects.order_by('-pub_date')[:5]
+    output = ', '.join([p.question_text for p in latest_question_list])
+    return HttpResponse(output)
+
 
 
 def index(request):
     return HttpResponse("Hello, world. You're at the polls index.")
+
 def detail(request, question_id):
     return HttpResponse("You're looking at question %s." % question_id)
 
