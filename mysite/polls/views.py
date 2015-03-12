3d8bad917fd029e754547ede5985cb09d72e7aed
annchab93 <Anncha@pc166-44.hiof.no>
2015-03-12 09:49:20 +0100

    jeg tror jeg er ferdig

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -9,10 +9,6 @@ def index(request):
     context = {'latest_question_list': latest_question_list}
     return render(request, 'polls/index.html', context)
 
-
-def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
-
 def detail(request, question_id):
     return HttpResponse("You're looking at question %s." % question_id)
 
