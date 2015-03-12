054b789174189f87f21e1d1b26bcc9394891a282
annchab93 <Anncha@pc166-44.hiof.no>
2015-03-12 09:21:40 +0100

    list,urls

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render
 
 from polls.models import Question
 from django.http import HttpResponse
@@ -29,4 +29,8 @@ def detail(request, question_id):
         question = Question.objects.get(pk=question_id)
     except Question.DoesNotExist:
         raise Http404("Question does not exist")
+    return render(request, 'polls/detail.html', {'question': question})
+
+def detail(request, question_id):
+    question = get_object_or_404(Question, pk=question_id)
     return render(request, 'polls/detail.html', {'question': question})
