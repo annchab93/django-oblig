c716019ed03204de5d5ea2bc36fd5d3f7879942f
annchab93 <Anncha@pc166-44.hiof.no>
2015-03-12 08:41:29 +0100

    templates&stuff

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,16 +1,13 @@
 from django.shortcuts import render
 
-# Create your views here.
+from polls.models import Question
 from django.http import HttpResponse
-from polls.models import Question 
-
 
 
 def index(request):
     latest_question_list = Question.objects.order_by('-pub_date')[:5]
-    output = ', '.join([p.question_text for p in latest_question_list])
-    return HttpResponse(output)
-
+    context = {'latest_question_list': latest_question_list}
+    return render(request, 'polls/index.html', context)
 
 
 def index(request):
@@ -25,3 +22,11 @@ def results(request, question_id):
 
 def vote(request, question_id):
     return HttpResponse("You're voting on question %s." % question_id)
+
+
+def detail(request, question_id):
+    try:
+        question = Question.objects.get(pk=question_id)
+    except Question.DoesNotExist:
+        raise Http404("Question does not exist")
+    return render(request, 'polls/detail.html', {'question': question})
